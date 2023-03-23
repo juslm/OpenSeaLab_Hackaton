9f5f87790a9be22f0e824a59eaf2e1d6de1ce97a
Lucmeister55 <96026889+Lucmeister55@users.noreply.github.com>
2023-03-23 14:29:13 +0100

    Update script.py

--- script.py
+++ script.py
@@ -6,3 +6,4 @@ offshore_installations = pd.read_csv(r'data/offshore_installations.csv')
 ports = pd.read_csv(r'data/ports.csv')
 shipwrecks = pd.read_csv(r'data/shipwrecks.csv')
 windfarms = pd.read_csv(r'data/windfarms.csv')
+
