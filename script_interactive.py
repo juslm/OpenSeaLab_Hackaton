4039e15da1ca013639c3143d817e1e6dbb6fb2a2
Lucmeister55 <96026889+Lucmeister55@users.noreply.github.com>
2023-03-26 14:24:33 +0200

    update

--- script_interactive.py
+++ script_interactive.py
@@ -3,13 +3,16 @@ import pandas as pd
 import geopandas as gpd
 import folium
 from folium.plugins import HeatMap
+from folium.plugins import MousePosition
 from shapely.geometry import Polygon
 
 from requests import Request
 import requests
 from owslib.wfs import WebFeatureService
+from owslib.wms import WebMapService
 import os
 import webbrowser
+import urllib.parse
 
 def get_humanact(name):
     # URL for WFS backend
@@ -33,26 +36,34 @@ def get_humanact(name):
 
     return gdf
 
-points_data = ["munitions", "platforms"]
-poly_data = ["windfarmspoly"]
+xmin = -30
+xmax = 40
+ymin = 28
+ymax = 73
+
+munitions_buffer = 5000
+platforms_buffer = 5000
+windfarmspoly_buffer = 0
+
+data = ["munitions", "platforms", "windfarmspoly"]
+buffers = {}
+
+buffers["munitions"] = munitions_buffer
+buffers["platforms"] = platforms_buffer
+buffers["windfarmspoly"] = windfarmspoly_buffer
 
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
 coordinates = world["geometry"]
-
-xmin = -30
-xmax = 55
-ymin = 28
-ymax = 73
 
 safe = gpd.GeoSeries(Polygon([[xmin, ymin], [xmin, ymax], [xmax, ymax], [xmax, ymin]]), crs = 4326).difference(world.geometry.unary_union)
 safe = safe.to_crs("EPSG:32634")
 
 colors = ['red', 'purple', 'blue', 'yellow']
 
-m = folium.Map(zoom_start = 5, location = [51.505, -0.09])
+m = folium.Map(zoom_start = 5, location = [51.505, -0.09], control_scale = True)
 
-for i, layer in enumerate(points_data):
+for i, layer in enumerate(data):
     ha = get_humanact(layer).to_crs("EPSG:4326")
     ha["layer"] = pd.Series([layer for x in range(len(ha.index))])
     ha.explore(
@@ -60,28 +71,17 @@ for i, layer in enumerate(points_data):
      color=colors[i],
      name=layer
     )
-    circles = ha.to_crs("EPSG:32634").geometry.buffer(1000)
+    circles = ha.to_crs("EPSG:32634").geometry.buffer(buffers[layer])
     mp = circles.unary_union
     safe = safe.difference(mp)
-
-for j, layer in enumerate(poly_data):
-    ha = get_humanact(layer).to_crs("EPSG:4326")
-    ha["layer"] = pd.Series([layer for x in range(len(ha.index))])
-    ha.explore(
-     m=m,
-     color=colors[i+1+j],
-     name=layer
-    )
-    circles = ha.to_crs("EPSG:32634").geometry
-    mp = circles.unary_union
-    safe = safe.difference(mp)
-
-folium.LayerControl().add_to(m)
 
 safe = gpd.GeoDataFrame(geometry = safe)
 safe.explode()
 
-folium.GeoJson(data=safe.geometry).add_to(m)
+folium.GeoJson(data=safe.geometry, style_function=lambda x:{"fillColor":"green", "color":"green"}, name = "safe").add_to(m)
+
+folium.LayerControl().add_to(m)
+MousePosition().add_to(m)
 
 m.save('my_map.html')
 webbrowser.open('file://' + os.path.realpath('my_map.html'))
