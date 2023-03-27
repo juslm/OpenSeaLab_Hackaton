9856731a45dfc89e1e2cf9476ceb2ab1441c9053
Lucmeister55 <96026889+Lucmeister55@users.noreply.github.com>
2023-03-27 18:03:54 +0200

    update

--- backend/data_collect.py
+++ backend/data_collect.py
@@ -19,6 +19,25 @@ def get_windfarmspoly(name="windfarmspoly"):
     ha = get_humanact(name).to_crs("EPSG:4326")
     return ha.to_json()
 
+def get_windspeedgrid(min_speed = 4, max_speed = 15):
+    windspeeds = pd.read_csv("data/windspeeds.csv", names = ["geometry", "speed(m/s)"])
+    windspeeds["geometry"] = [Point(eval(s)) for s in windspeeds["geometry"]]
+    windspeeds["weight"] = [n/max(windspeeds["speed(m/s)"]) for n in windspeeds["speed(m/s)"]]
+    windspeeds = gpd.GeoDataFrame(windspeeds)
+
+    tiles = []
+
+    dist = abs(windspeeds.geometry[0].xy[1][0]-windspeeds.geometry[1].xy[1][0])
+
+    for i, point in enumerate(windspeeds.geometry):
+        x = point.xy[0][0]
+        y = point.xy[1][0]
+        tiles.append(Polygon([[x-dist/2, y+dist/2], [x+dist/2, y+dist/2], [x+dist/2, y-dist/2], [x-dist/2, y-dist/2]]))
+    
+    windspeeds["tiles"] = gpd.GeoSeries(tiles, crs = "EPSG:4326")
+    windspeeds = windspeeds[(min_speed <= windspeeds["speed(m/s)"]) & (max_speed >= windspeeds["speed(m/s)"])].reset_index()
+
+    return [windspeeds.to_json(), windspeeds]
 
 def get_safe(xmin=-30,
              xmax=40,
@@ -32,19 +51,17 @@ def get_safe(xmin=-30,
         data = ["munitions", "platforms", "windfarmspoly"]
     buffers = {"munitions": munitions_buffer, "platforms": platforms_buffer, "windfarmspoly": windfarmspoly_buffer}
 
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    water = gpd.GeoSeries(Polygon([[xmin, ymin], [xmin, ymax], [xmax, ymax], [xmax, ymin]]), crs=4326).difference(
-        world.geometry.unary_union)
-    safe = water.to_crs("EPSG:32634")
+    windspeeds = get_windspeedgrid()[1]
+
+    safe = windspeeds["tiles"].to_crs("EPSG:32634")
+    safe = safe.unary_union
 
     for i, layer in enumerate(data):
         ha = get_humanact(layer).to_crs("EPSG:4326")
-        ha["layer"] = pd.Series([layer for x in range(len(ha.index))])
         circles = ha.to_crs("EPSG:32634").geometry.buffer(buffers[layer])
         mp = circles.unary_union
         safe = safe.difference(mp)
 
-    safe = gpd.GeoDataFrame(geometry=safe)
-    safe.explode()
+    safe = gpd.GeoDataFrame(geometry = gpd.GeoSeries(safe), crs = "EPSG:32634")
 
     return safe.to_json()
