406dde0f86b3d2333e5dcbab3fbc1a76d435244f
Lucmeister55 <96026889+Lucmeister55@users.noreply.github.com>
2023-03-27 11:07:47 +0200

    update

--- script_interactive.py
+++ script_interactive.py
@@ -5,6 +5,7 @@ import folium
 from folium.plugins import HeatMap
 from folium.plugins import MousePosition
 from shapely.geometry import Polygon
+from shapely.geometry import Point
 
 from requests import Request
 import requests
@@ -36,6 +37,22 @@ def get_humanact(name):
 
     return gdf
 
+windspeeds = pd.read_csv("data/windspeeds.csv", names = ["geometry", "speed(kph)"])
+windspeeds["geometry"] = [Point(eval(s)) for s in windspeeds["geometry"]]
+windspeeds["weight"] = [n/max(windspeeds["speed(kph)"]) for n in windspeeds["speed(kph)"]]
+windspeeds = gpd.GeoDataFrame(windspeeds)
+
+tiles = []
+
+dist = abs(windspeeds.geometry[0].xy[1][0]-windspeeds.geometry[1].xy[1][0])
+
+for i, point in enumerate(windspeeds.geometry):
+    x = point.xy[0][0]
+    y = point.xy[1][0]
+    tiles.append(Polygon([[x-dist/2, y+dist/2], [x+dist/2, y+dist/2], [x+dist/2, y-dist/2], [x-dist/2, y-dist/2]]))
+
+windspeeds["tiles"] = tiles
+
 xmin = -30
 xmax = 40
 ymin = 28
@@ -66,11 +83,12 @@ m = folium.Map(zoom_start = 5, location = [51.505, -0.09], control_scale = True)
 for i, layer in enumerate(data):
     ha = get_humanact(layer).to_crs("EPSG:4326")
     ha["layer"] = pd.Series([layer for x in range(len(ha.index))])
-    ha.explore(
-     m=m,
-     color=colors[i],
-     name=layer
-    )
+    # ha.explore(
+    #  m=m,
+    #  color=colors[i],
+    #  name=layer
+    # )
+    ha.explore(m=m, color=colors[i], name=layer, show = False)
     circles = ha.to_crs("EPSG:32634").geometry.buffer(buffers[layer])
     mp = circles.unary_union
     safe = safe.difference(mp)
@@ -79,6 +97,16 @@ safe = gpd.GeoDataFrame(geometry = safe)
 safe.explode()
 
 folium.GeoJson(data=safe.geometry, style_function=lambda x:{"fillColor":"green", "color":"green"}, name = "safe").add_to(m)
+
+# w = folium.FeatureGroup("wind speed")
+# for i, point in enumerate(windspeeds.geometry):
+#     folium.Marker([point.xy[1][0], point.xy[0][0]], popup=windspeeds["speed(kph)"][i]).add_to(m)
+# m.add_child(w)
+
+# wind_heat_data = [[point.xy[1][0], point.xy[0][0]] for point in windspeeds.geometry]
+# HeatMap(wind_heat_data, name = layer, max_zoom = 2, blur = 0).add_to(m)
+
+folium.GeoJson(data=gpd.GeoSeries(windspeeds["tiles"], crs = "EPSG:4326"), style_function=lambda x:{"fillColor":"white", "color":"black"}, name = "wind", popup = windspeeds["speed(kph)"]).add_to(m)
 
 folium.LayerControl().add_to(m)
 MousePosition().add_to(m)
