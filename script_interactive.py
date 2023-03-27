1dc2e5e9c6cae934a34ec01de285450a41ff04e1
Lucmeister55 <96026889+Lucmeister55@users.noreply.github.com>
2023-03-27 12:12:17 +0200

    update

--- script_interactive.py
+++ script_interactive.py
@@ -37,9 +37,9 @@ def get_humanact(name):
 
     return gdf
 
-windspeeds = pd.read_csv("data/windspeeds.csv", names = ["geometry", "speed(kph)"])
+windspeeds = pd.read_csv("data/windspeeds.csv", names = ["geometry", "speed(m/s)"])
 windspeeds["geometry"] = [Point(eval(s)) for s in windspeeds["geometry"]]
-windspeeds["weight"] = [n/max(windspeeds["speed(kph)"]) for n in windspeeds["speed(kph)"]]
+windspeeds["weight"] = [n/max(windspeeds["speed(m/s)"]) for n in windspeeds["speed(m/s)"]]
 windspeeds = gpd.GeoDataFrame(windspeeds)
 
 tiles = []
@@ -51,7 +51,7 @@ for i, point in enumerate(windspeeds.geometry):
     y = point.xy[1][0]
     tiles.append(Polygon([[x-dist/2, y+dist/2], [x+dist/2, y+dist/2], [x+dist/2, y-dist/2], [x-dist/2, y-dist/2]]))
 
-windspeeds["tiles"] = tiles
+windspeeds["tiles"] = gpd.GeoSeries(tiles, crs = "EPSG:4326")
 
 xmin = -30
 xmax = 40
@@ -61,6 +61,9 @@ ymax = 73
 munitions_buffer = 5000
 platforms_buffer = 5000
 windfarmspoly_buffer = 0
+
+min_speed = 4
+max_speed = 15
 
 data = ["munitions", "platforms", "windfarmspoly"]
 buffers = {}
@@ -73,8 +76,8 @@ world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
 coordinates = world["geometry"]
 
-safe = gpd.GeoSeries(Polygon([[xmin, ymin], [xmin, ymax], [xmax, ymax], [xmax, ymin]]), crs = 4326).difference(world.geometry.unary_union)
-safe = safe.to_crs("EPSG:32634")
+water = gpd.GeoSeries(Polygon([[xmin, ymin], [xmin, ymax], [xmax, ymax], [xmax, ymin]]), crs = 4326).difference(world.geometry.unary_union)
+safe = water.to_crs("EPSG:32634")
 
 colors = ['red', 'purple', 'blue', 'yellow']
 
@@ -83,11 +86,6 @@ m = folium.Map(zoom_start = 5, location = [51.505, -0.09], control_scale = True)
 for i, layer in enumerate(data):
     ha = get_humanact(layer).to_crs("EPSG:4326")
     ha["layer"] = pd.Series([layer for x in range(len(ha.index))])
-    # ha.explore(
-    #  m=m,
-    #  color=colors[i],
-    #  name=layer
-    # )
     ha.explore(m=m, color=colors[i], name=layer, show = False)
     circles = ha.to_crs("EPSG:32634").geometry.buffer(buffers[layer])
     mp = circles.unary_union
@@ -98,15 +96,12 @@ safe.explode()
 
 folium.GeoJson(data=safe.geometry, style_function=lambda x:{"fillColor":"green", "color":"green"}, name = "safe").add_to(m)
 
-# w = folium.FeatureGroup("wind speed")
-# for i, point in enumerate(windspeeds.geometry):
-#     folium.Marker([point.xy[1][0], point.xy[0][0]], popup=windspeeds["speed(kph)"][i]).add_to(m)
-# m.add_child(w)
-
-# wind_heat_data = [[point.xy[1][0], point.xy[0][0]] for point in windspeeds.geometry]
-# HeatMap(wind_heat_data, name = layer, max_zoom = 2, blur = 0).add_to(m)
-
-folium.GeoJson(data=gpd.GeoSeries(windspeeds["tiles"], crs = "EPSG:4326"), style_function=lambda x:{"fillColor":"white", "color":"black"}, name = "wind", popup = windspeeds["speed(kph)"]).add_to(m)
+w = folium.FeatureGroup(name='wind speed').add_to(m)
+for i, row in windspeeds.iterrows():
+    if min_speed < row["speed(m/s)"] < max_speed:# and not row["tiles"].within(world.geometry):
+        b = folium.GeoJson(data=row["tiles"], style_function=lambda x:{"fillColor":"white", "color":"black"})
+        b.add_child(folium.Popup(str(row["speed(m/s)"])))
+        w.add_child(b)
 
 folium.LayerControl().add_to(m)
 MousePosition().add_to(m)
